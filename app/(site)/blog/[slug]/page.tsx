0211fd3750170a22634ab37577e299364075cc9b
alonkeyval <alon@keyval.dev>
2023-09-21 16:32:01 +0300

    chore: wip

--- app/(site)/blog/[slug]/page.tsx
+++ app/(site)/blog/[slug]/page.tsx
@@ -1,127 +1,85 @@
+'use client';
 import Image from 'next/image';
 import { getPost, imageBuilder } from '@/sanity/sanity-utils';
 import { PortableText } from '@portabletext/react';
 import SharePost from '@/components/Blog/SharePost';
 import RelatedPost from '@/components/Blog/RelatedPost';
+import styled from 'styled-components';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
+const markdown = `As observability grows in popularity, new open-source solutions continue to emerge, allowing companies to implement end-to-end observability solutions without spending a dime.
+
+This growing trend of observability open-source software is in line with developments across other technology sectors. Already, web, mobile, and cloud solutions are increasingly built on open-source infrastructure. Future architectures are highly likely to be based on open source, as they are today in mobile solutions (Android platform), web solutions, where most websites are based on open-source technology; and cloud solutions, where almost all are based on open-source solutions as well.
+
+Observability vendors offer solutions that process signals (distributed traces, metrics & logs) that are displayed on dashboards with performance KPI’s and insights reflecting the health and security of any applications. The challenge has been implementing these technologies to deliver their full potential of observability capabilities. Looking at the “getting started” page of most observability vendors, it is clear why implementing these solutions is a challenging process that includes: changing code, using SDK’s to auto-instrument applications, configuring & deploying collectors and finally, continuously managing them to scale (for new applications, new collectors, etc.).
+
+Enter Odigos – an open-source, agnostic observability control plane that automates the whole process. With a quick & easy installation replacing days, if not weeks, of integration, companies can have all their application produce distributed tracing, metrics and logs within minutes. As an agnostic observability enabler, Odigos allows you to connect to any observability tool vendor out there, open-source or not.
+
+Being big believers, supporters, and maintainers in the open-source community, Odigos is happy to announce that we are **now integrated with five open-source observability tools vendors.**
+
+**Prometheus, Jaeger, Loki, Tempo & Signoiz** are all now supported by Odigos and can be installed within minutes.
+
+## What does this mean
+
+It means companies have **a choice** of vendors when implementing an end-to-end open-source observability solution. As shown in the table below, between the five vendors there are currently three different open-source solutions for processing and analyzing Distributed Tracing, Metrics & Logs.
+
+|            | Traces | Metrics | Logs |
+| ---------- | ------ | ------- | ---- |
+| Prometheus |        | ✅      |      |
+| Tempo      | ✅     |         |      |
+| Loki       |        |         | ✅   |
+| Jaeger     | ✅     |         |      |
+| SigNoz     | ✅     | ✅      | ✅   |
+
+Additionally, it’s now easy for companies can work with more than one Observability vendor. Do you like Prometheus for you Metrics but want to use Tempo for your distributed tracing? Not a problem. As the industry continues to grow and observability becomes ubiquitous, additional tools with new functionality are flourishing (Security, etc.) and we expect companies in the future to use more the one observability vendors.
+
+This is just a first step. Odigos will continue to integrate open-source solutions, allowing our customers a gateway to choose the solution that best fits their needs. As additional functionality is developed, Odigos will continue supporting multiple vendors enabling companies to prepare and take advantage of the new observability-based solutions being developed
+
+## The open-source vendors supported by Odigos
+
+**Prometheus** – Prometheus is an open-source software application used for event monitoring and alerting. It records real-time metrics in a time series database built using a HTTP pull model, with flexible queries and real-time alerting.
+
+**Jaeger** – Jaeger is open-source software for tracing transactions between distributed services. It's used for monitoring and troubleshooting complex microservices environments
+
+**Loki** - Loki is a log aggregation system designed to store and query logs from all your applications and infrastructure.
+
+**Tempo** - Tempo is an open source, easy-to-use, and high-scale distributed tracing backend.
+
+**SigNoz** - Supports all three telemetry signals under a single dashboard. You can correlate your application metrics and traces, and use it for things like infrastructure monitoring
+`;
 type Props = {
   params: { slug: string };
 };
 
+const TextWrapper = styled.div`
+  code {
+    background-color: #f6f8fa;
+    display: block;
+    border-radius: 4px;
+    padding: 1rem;
+    color: #1f2328;
+  }
+`;
+
 const SingleBlogPage = async ({ params }: Props) => {
   const { slug } = params;
-  const post = await getPost(slug);
-
+  // const post = await getPost(slug);
+  // console.log(post.body);
   return (
     <>
-      <title>{`${post?.title} - Solid`}</title>
       <section className="pt-35 lg:pt-45 xl:pt-50 pb-20 lg:pb-25 xl:pb-30">
         <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
           <div className="flex flex-col-reverse lg:flex-row gap-7.5 xl:gap-12.5">
-            <div className="md:w-1/2 lg:w-[32%]">
-              <div className="animate_top rounded-md shadow-solid-13 bg-white dark:bg-blacksection border border-stroke dark:border-strokedark p-3.5 mb-10">
-                <form
-                  action="https://formbold.com/s/unique_form_id"
-                  method="POST"
-                >
-                  <div className="relative">
-                    <input
-                      type="text"
-                      placeholder="Search Here..."
-                      className="w-full dark:bg-black border border-stroke dark:border-strokedark shadow-solid-12 dark:shadow-none rounded-lg focus:outline-none focus:border-primary dark:focus:border-primary py-4 px-6"
-                    />
-
-                    <button
-                      className="absolute top-0 right-0 p-5"
-                      aria-label="search-icon"
-                    >
-                      <svg
-                        className="fill-black dark:fill-white hover:fill-primary dark:hover:fill-primary transition-all duration-300"
-                        width="21"
-                        height="21"
-                        viewBox="0 0 21 21"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path d="M16.031 14.617L20.314 18.899L18.899 20.314L14.617 16.031C13.0237 17.3082 11.042 18.0029 9 18C4.032 18 0 13.968 0 9C0 4.032 4.032 0 9 0C13.968 0 18 4.032 18 9C18.0029 11.042 17.3082 13.0237 16.031 14.617ZM14.025 13.875C15.2941 12.5699 16.0029 10.8204 16 9C16 5.132 12.867 2 9 2C5.132 2 2 5.132 2 9C2 12.867 5.132 16 9 16C10.8204 16.0029 12.5699 15.2941 13.875 14.025L14.025 13.875Z" />
-                      </svg>
-                    </button>
-                  </div>
-                </form>
-              </div>
-
-              <div className="animate_top rounded-md shadow-solid-13 bg-white dark:bg-blacksection border border-stroke dark:border-strokedark p-9 mb-10">
-                <h4 className="font-semibold text-2xl text-black dark:text-white mb-7.5">
-                  Categories
-                </h4>
-
-                <ul>
-                  <li className="last:mb-0 mb-3 transition-all duration-300 hover:text-primary">
-                    <a href="#">Blog</a>
-                  </li>
-                  <li className="last:mb-0 mb-3 transition-all duration-300 hover:text-primary">
-                    <a href="#">Events</a>
-                  </li>
-                  <li className="last:mb-0 mb-3 transition-all duration-300 hover:text-primary">
-                    <a href="#">Grids</a>
-                  </li>
-                  <li className="last:mb-0 mb-3 transition-all duration-300 hover:text-primary">
-                    <a href="#">News</a>
-                  </li>
-                  <li className="last:mb-0 mb-3 transition-all duration-300 hover:text-primary">
-                    <a href="#">Rounded</a>
-                  </li>
-                </ul>
-              </div>
-
-              <RelatedPost />
-            </div>
-
             <div className="lg:w-2/3">
               <div className="animate_top rounded-md shadow-solid-13 bg-white dark:bg-blacksection border border-stroke dark:border-strokedark p-7.5 md:p-10">
-                <div className="mb-10 w-full overflow-hidden ">
-                  {post?.mainImage && (
-                    <div className="relative aspect-[97/60] w-full sm:aspect-[97/44]">
-                      <Image
-                        src={imageBuilder(post?.mainImage).url()}
-                        alt={post?.title}
-                        fill
-                        className="object-cover object-center rounded-md"
-                      />
-                    </div>
-                  )}
-                </div>
-
-                <h2 className="font-semibold text-3xl 2xl:text-sectiontitle2 text-black dark:text-white mt-11 mb-5">
-                  {post?.title}
-                </h2>
-
-                <ul className="flex flex-wrap gap-5 2xl:gap-7.5 mb-9">
-                  <li>
-                    <span className="text-black dark:text-white">Author: </span>{' '}
-                    {post?.author?.name}
-                  </li>
-                  <li>
-                    <span className="text-black dark:text-white">
-                      Published On:{' '}
-                    </span>{' '}
-                    {new Date(post?.publishedAt)
-                      .toDateString()
-                      .split(' ')
-                      .slice(1)
-                      .join(' ')}
-                  </li>
-                  <li>
-                    <span className="text-black dark:text-white">
-                      Category:{' '}
-                    </span>{' '}
-                    {post?.tags ? post?.tags[0] : 'Not added'}
-                  </li>
-                </ul>
-
                 <div className="blog-details">
-                  <PortableText value={post?.body} />
+                  {/* <PortableText value={post?.body} /> */}
                 </div>
+                <ReactMarkdown
+                  children={markdown}
+                  remarkPlugins={[remarkGfm]}
+                />
 
                 <SharePost />
               </div>
