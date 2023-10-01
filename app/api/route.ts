55a7a088dd99da3714e83d62e6069d35119f41ab
alonkeyval <alon@keyval.dev>
2023-10-01 13:50:33 +0300

    chore(build): fixed build issue

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,12 +1,12 @@
-import { getServerSession } from "next-auth/next";
-import { NextResponse } from "next/server";
-import { authOptions } from "./auth/[...nextauth]/route";
+import { getServerSession } from 'next-auth/next';
+import { NextResponse } from 'next/server';
+import { authOptions } from './auth/[...nextauth]/route';
 
 export async function GET(request: Request) {
   const session = await getServerSession(authOptions);
 
   if (!session) {
-    return new NextResponse(JSON.stringify({ error: "unauthorized" }), {
+    return new NextResponse(JSON.stringify({ error: 'unauthorized' }), {
       status: 401,
     });
   }
