1aa9ca223a0cb80249eb79266ca1f1793156617f
alonkeyval <alon@keyval.dev>
2023-10-05 15:53:55 +0300

    chore(analitycs): add plausible snippets

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -2,6 +2,11 @@ export default function Head() {
   return (
     <>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <script
+        defer
+        data-domain="odigos.io"
+        src="https://plausible.io/js/script.js"
+      ></script>
     </>
   );
 }
