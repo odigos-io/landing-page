39ff9a686fc4141b8c47f01af6f8ac7a2e5e16c9
alonkeyval <alon@keyval.dev>
2023-10-15 15:56:33 +0300

    chore: send to services

--- components/WaitlistForm/utils.ts
+++ components/WaitlistForm/utils.ts
@@ -1,6 +1,30 @@
 const WAIT_LIST_API_URL =
   'https://2t0oabc9g3.execute-api.us-east-2.amazonaws.com/default/setWaitListItem';
 
+const SLACK_API_URL =
+  'https://edszwqsqg2.execute-api.us-east-2.amazonaws.com/default/waitlist-post-slack-channel-message';
+
+const MAILCHIMP_API_URL =
+  'https://jww8cul3t6.execute-api.us-east-2.amazonaws.com/default/mailchimp-add-member-to-audience';
+
+async function sendToService(
+  body: { name: string; email: string },
+  url: string
+) {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        accept: 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+    console.log({ response });
+  } catch (error) {
+    console.log({ error });
+  }
+}
+
 export async function setWaitListItem({
   name,
   email,
@@ -12,17 +36,12 @@ export async function setWaitListItem({
     name,
     email,
   };
+
   try {
-    const response = await fetch(WAIT_LIST_API_URL, {
-      method: 'POST',
-      headers: {
-        accept: 'application/json',
-      },
-      body: JSON.stringify(body),
-    });
-    if (response.status === 200) {
-      return true;
-    }
+    await sendToService(body, SLACK_API_URL);
+    await sendToService(body, MAILCHIMP_API_URL);
+    await sendToService(body, WAIT_LIST_API_URL);
+    return true;
   } catch (error) {
     return false;
   }
