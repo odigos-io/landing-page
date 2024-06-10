cc668528ba1152e1c1c89489efdd91e8ad6911cd
alonkeyval <alon@keyval.dev>
2024-06-10 16:07:51 +0300

    chore: wip

---
+++ containers/about/principles/data.ts
@@ -0,0 +1,38 @@
+const DATA = [
+  {
+    title: 'Innovation',
+    icon: '',
+    description:
+      'We thrive on technology and innovation, always exploring new frontiers to deliver cutting-edge solutions.',
+  },
+  {
+    title: 'Open Source',
+    icon: '',
+    description:
+      'We proudly contribute to the open-source community, fostering collaboration and continuous learning.',
+  },
+  {
+    title: 'Customer Focus',
+    icon: '',
+    description:
+      'Our customers are at the heart of everything we do. We aim to understand their needs and deliver solutions that drive their success.',
+  },
+  {
+    title: 'Team Spirit',
+    icon: '',
+    description:
+      'Our young, dynamic team thrives on creativity, collaboration, and fun. We value every idea and empower every team member.',
+  },
+  {
+    title: 'Excellence',
+    icon: '',
+    description:
+      'We maintain the highest standards in all our endeavors, ensuring enterprise-level quality, reliability, and scalability.',
+  },
+  {
+    title: 'Simplicity',
+    icon: '',
+    description:
+      'We strive to make our solutions simple and intuitive, removing complexity to ensure ease of use for our customers.',
+  },
+];
