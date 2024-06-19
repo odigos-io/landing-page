8d46075c10c1a978d90b745138bca5686ccc7b54
alonkeyval <alon@keyval.dev>
2024-06-19 15:32:01 +0300

    chore: init team

---
+++ containers/about/team/data.ts
@@ -0,0 +1,51 @@
+export const DATA = [
+  {
+    name: 'Eden Smith',
+    title: 'Chief Executive Officer',
+    description:
+      'Eden leads the vision and strategy at Odigos. With over 15 years of experience in tech startups.',
+    linkedin_url: 'https://www.linkedin.com/in/edensmith',
+  },
+  {
+    name: 'Air Johnson',
+    title: 'Chief Technology Officer',
+    description:
+      'Air is responsible for the technological direction at Odigos. With a background in software engineering, Air specializes in scalable architecture and system design, With a background in software engineering, Air specializes in scalable architecture and system design.With a background in software engineering, Air specializes in scalable architecture and system design.With a background in software engineering, Air specializes in scalable architecture and system design.',
+    linkedin_url: 'https://www.linkedin.com/in/airjohnson',
+  },
+  {
+    name: 'Along Williams',
+    title: 'VP of Product',
+    description:
+      "Along oversees the product development and management at Odigos. Along's expertise lies in creating user-centric products that meet market needs.",
+    linkedin_url: 'https://www.linkedin.com/in/alongwilliams',
+  },
+  {
+    name: 'Amir Brown',
+    title: 'Head of Marketing',
+    description:
+      'Amir drives the marketing efforts at Odigos, bringing over a decade of experience in digital marketing and brand strategy.',
+    linkedin_url: 'https://www.linkedin.com/in/amirbrown',
+  },
+  {
+    name: 'Ron Davis',
+    title: 'Chief Operating Officer',
+    description:
+      'Ron ensures operational excellence at Odigos. With a keen eye for detail, Ron manages the day-to-day operations and long-term strategic planning.',
+    linkedin_url: 'https://www.linkedin.com/in/rondavis',
+  },
+  {
+    name: 'Tamie Wilson',
+    title: 'Chief Financial Officer',
+    description:
+      'Tamie oversees the financial health of Odigos. With extensive experience in financial planning and analysis, Tamie ensures sustainable growth and profitability.',
+    linkedin_url: 'https://www.linkedin.com/in/tamiewilson',
+  },
+  {
+    name: 'David Clark',
+    title: 'Head of Customer Success',
+    description:
+      'David is dedicated to ensuring customer satisfaction at Odigos. David brings a wealth of experience in customer relationship management and support.',
+    linkedin_url: 'https://www.linkedin.com/in/davidclark',
+  },
+];
