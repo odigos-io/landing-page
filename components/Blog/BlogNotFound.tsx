3c6d479dc96ab3099106cfb89b64ee5f460f6869
alonkeyval <alon@keyval.dev>
2024-07-02 08:54:30 +0300

    chore: wip

--- components/Blog/BlogNotFound.tsx
+++ components/Blog/BlogNotFound.tsx
@@ -1,54 +1,8 @@
 'use client';
-import styled from 'styled-components';
 import React from 'react';
-import { Button } from '@/design-system/button/button';
-import { Text } from '@/design-system/text/text';
-
-const Container = styled.div`
-  height: 100%;
-  width: 100%;
-  padding: 200px 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Title = styled.h1`
-  color: #fff;
-`;
-
-const Description = styled.p`
-  color: #fff;
-`;
-
-const StyledButton = styled(Button)`
-  margin-top: 40px;
-  width: 200px;
-  height: 50px;
-`;
-
-const Divider = styled.div`
-  height: 30px;
-`;
 
 const BlogNotFound: React.FC = () => {
-  return (
-    <Container>
-      <Title>Blog Not Found</Title>
-      <Description>Sorry, the requested blog could not be found.</Description>
-      <Divider />
-      <StyledButton
-        onClick={() => {
-          window.location.href = '/blog';
-        }}
-      >
-        <Text color={'#0A1824'} weight={600}>
-          Go Back Home
-        </Text>
-      </StyledButton>
-    </Container>
-  );
+  return <></>;
 };
 
 export default BlogNotFound;
