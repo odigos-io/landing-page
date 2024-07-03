428cdfd16fa27d719f9c10fdc3a0a706d81cc951
alonkeyval <alon@keyval.dev>
2024-07-03 15:26:55 +0300

    chore: init hero animation

--- containers/pricing/hero/index.tsx
+++ containers/pricing/hero/index.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import theme from '@/style/theme';
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 const PageContainer = styled.div`
   display: flex;
@@ -43,13 +43,159 @@ const Divider = styled.div`
   background: rgba(249, 249, 249, 0.64);
 `;
 
+const slide = keyframes`
+  0% {
+    transform: translateX(-100%);
+    height: 0;
+  }
+
+  1% {
+    opacity: 1;
+  }
+
+  20% {
+    height: 4px;
+  }
+
+  40% {
+    height: 10px;
+  }
+
+  60% {
+    height: 6px;
+  }
+
+  80% {
+    height: 4px;
+  }
+
+  100% {
+    transform: translateX(calc(100vw - 250px));
+    height: 0px;
+  }
+`;
+
+const slide2 = keyframes`
+  0% {
+    transform: translateX(-50%);
+    height: 0;
+  }
+
+  1% {
+    opacity: 1;
+  }
+
+  20% {
+    height: 4px;
+  }
+
+  40% {
+    height: 10px;
+  }
+
+  60% {
+    height: 6px;
+  }
+
+  80% {
+    height: 4px;
+  }
+
+  100% {
+    transform: translateX(calc(100vw - 250px));
+    height: 0px;
+    
+  }
+`;
+
+const slide3 = keyframes`
+  0% {
+    transform: translateX(0%);
+    height: 0;
+  }
+
+  1% {
+    opacity: 1;
+  }
+
+  20% {
+    height: 4px;
+  }
+
+  40% {
+    height: 10px;
+  }
+
+  60% {
+    height: 6px;
+  }
+
+  80% {
+    height: 4px;
+  }
+
+  100% {
+    transform: translateX(calc(100vw - 180px));
+    height: 0px;
+    
+  }
+`;
+
+const Span = styled.div<{ delay: number }>`
+  width: 100px;
+  background: rgba(249, 249, 249, 0.64);
+  background: ${({ theme }) => theme.colors.white};
+  opacity: 0;
+  animation: ${slide} 1.5s forwards;
+  ${({ delay }) =>
+    delay &&
+    css`
+      animation-delay: ${delay}s;
+    `}
+`;
+
+const Span2 = styled.div<{ delay: number }>`
+  width: 200px;
+  background: rgba(249, 249, 249, 0.64);
+  background: ${({ theme }) => theme.colors.white};
+  animation: ${slide2} 1s forwards;
+  ${({ delay }) =>
+    delay &&
+    css`
+      animation-delay: 0.5s;
+    `}
+`;
+
+const Span3 = styled.div<{ delay: number }>`
+  width: 150px;
+  background: rgba(249, 249, 249, 0.64);
+  background: ${({ theme }) => theme.colors.white};
+  animation: ${slide3} 1s forwards;
+  ${({ delay }) =>
+    delay &&
+    css`
+      animation-delay: 0.3s;
+    `}
+`;
+
 const PricingHero = () => {
   return (
     <PageContainer>
       <HeroTitle>Simple pricing, no surprises</HeroTitle>
       <DividersContainer>
         {[1, 2, 3, 4].map((_, key) => (
-          <Divider key={key} />
+          <div key={key}>
+            {key === 0 && (
+              <Span className="slide-span" delay={key > 0 ? key * 0.2 : 0} />
+            )}
+            {key === 2 && (
+              <Span2 className="slide-span" delay={key > 0 ? key * 0.2 : 0} />
+            )}
+            {key === 3 && (
+              <Span3 className="slide-span" delay={key > 0 ? key * 0.2 : 0} />
+            )}
+            <Divider />
+          </div>
         ))}
       </DividersContainer>
     </PageContainer>
