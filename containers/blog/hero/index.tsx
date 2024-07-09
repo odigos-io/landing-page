8640a3a2b6646029361a6c049f1557e0daab041e
alonkeyval <alon@keyval.dev>
2024-07-09 16:48:16 +0300

    chore: wip

--- containers/blog/hero/index.tsx
+++ containers/blog/hero/index.tsx
@@ -3,27 +3,32 @@ import React from 'react';
 import theme from '@/style/theme';
 import styled from 'styled-components';
 import TextHero from '@/components/text-hero';
+import { MaxWidthContainer } from '@/style';
 
 const PageContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 80px;
   background: ${theme.colors.secondary};
-  padding: 160px 64px 64px 64px;
+  padding: 160px 64px;
+  width: 100%;
+  max-width: 1440px;
   @media (max-width: 800px) {
     padding: 24px 20px;
-    padding-top: 160px;
   }
   @media (max-width: 600px) {
+    padding-top: 84px;
     gap: 32px;
   }
 `;
 
 const BlogHero = () => {
   return (
-    <PageContainer>
-      <TextHero text="Discover our latest articles and insights." />
-    </PageContainer>
+    <MaxWidthContainer>
+      <PageContainer>
+        <TextHero text="Discover our latest articles and insights." />
+      </PageContainer>
+    </MaxWidthContainer>
   );
 };
 
