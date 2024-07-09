2e399e923946525aff56bc46a637e2ebab424105
alonkeyval <alon@keyval.dev>
2024-07-09 13:55:35 +0300

    chore: wip

--- containers/how-we-do/index.tsx
+++ containers/how-we-do/index.tsx
@@ -34,7 +34,7 @@ const InnerBorder = styled.div`
 
 const Body = styled.div`
   display: flex;
-  padding: 160px 80px;
+  padding: 80px;
   flex-direction: column;
   justify-content: center;
   align-items: center;
