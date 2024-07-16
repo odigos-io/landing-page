c48e8bdd0eb1685e6210286f99093d61b7cf467e
alonkeyval <alon@keyval.dev>
2024-07-16 14:09:39 +0300

    chore: remove image from folder

--- public/social/index.tsx
+++
@@ -1,5 +0,0 @@
-import GoogleIcon from './google.svg';
-import GithubIcon from './github.svg';
-import LinkedinIcon from './linkedin.svg';
-import SlackIcon from './slack.svg';
-export { GoogleIcon, GithubIcon, LinkedinIcon, SlackIcon };
