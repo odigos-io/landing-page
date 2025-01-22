65daf9aca9d0535f2f024ba4d5c3ded0482893ac
Ben Elferink <ben.elferink@icloud.com>
2025-01-22 11:59:51 +0200

    feat: add dates to landing page blog

--- components/blogs/blog-footer/index.tsx
+++ components/blogs/blog-footer/index.tsx
@@ -1,27 +1,30 @@
-'use client';
-import theme from '@/style/theme';
-import { Text } from '@/reuseable-components';
-import { calculateReadingTime } from '@/utils';
+'use client'
+import theme from '@/style/theme'
+import { Text } from '@/reuseable-components'
+import { calculateReadingTime } from '@/utils'
+
+const COLOR = '#F5F5F576'
 
 export const BlogFooter = ({ blog }: { blog: any }) => {
-  const { tags, content } = blog;
+  const { tags, content, pubDate } = blog
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        gap: 16,
-      }}
-    >
-      <Text color="#f5f5f576" fontFam={theme.font_family.secondary}>
+    <div style={{ display: 'flex', gap: 16 }}>
+      <Text color={COLOR} fontFam={theme.font_family.secondary}>
         {tags?.[0]?.toUpperCase() || 'OBSERVABILITY'}
       </Text>
-      <Text color="#f5f5f576" fontFam={theme.font_family.secondary}>
+      <Text color={COLOR} fontFam={theme.font_family.secondary}>
         {'|'}
       </Text>
-      <Text color="#f5f5f576" fontFam={theme.font_family.secondary}>
+      <Text color={COLOR} fontFam={theme.font_family.secondary}>
         {`${calculateReadingTime(content)} MIN READ` || ''}
       </Text>
+      <Text color={COLOR} fontFam={theme.font_family.secondary}>
+        {'|'}
+      </Text>
+      <Text color={COLOR} fontFam={theme.font_family.secondary}>
+        {new Date(pubDate).toDateString() || '-'}
+      </Text>
     </div>
-  );
-};
+  )
+}
