c0b9fb6c5a5eb485b30b0f46bf05b8a70f5a9a21
alonkeyval <alon@odigos.io>
2025-04-27 14:37:53 +0300

    chore: wip

--- app/(site)/pricing/page.tsx
+++ app/(site)/pricing/page.tsx
@@ -2,12 +2,11 @@ import { Metadata } from 'next';
 import theme from '@/style/theme';
 import dynamic from 'next/dynamic';
 import PricingHero from '@/containers/pricing/hero';
+import PricingTable from '@/containers/pricing/pricing-table/pricing-table';
 
 const CTASection = dynamic(() => import('@/containers/cta'));
 const FAQComponent = dynamic(() => import('@/containers/pricing/faq'));
-const PricingComponent = dynamic(
-  () => import('@/containers/pricing/pricing-table')
-);
+const PricingComponent = dynamic(() => import('@/containers/pricing/pricing-table'));
 export const metadata: Metadata = {
   metadataBase: new URL('https://odigos.io'),
   title: 'Odigos - Instant Distributed Tracing',
@@ -22,6 +21,7 @@ export default function PricingPage() {
   return (
     <main style={{ background: theme.colors.secondary }}>
       <PricingHero />
+      <PricingTable />
       <PricingComponent />
       <FAQComponent />
       <CTASection />
