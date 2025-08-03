ae010450288467391966b9025ea9abc944685dff
Ben Elferink <ben@odigos.io>
2025-08-03 17:13:07 +0300

    refactor: cleanup codebase

--- components/blogs/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './blog-cover';
-export * from './blog-footer';
-export * from './blog-item';
-export * from './blog-not-found';
-export * from './blogs-list';
---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    styledComponents: true,
+  },
+};
+
+export default nextConfig;
