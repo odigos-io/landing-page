ec2c160de3e0d2d0aa5f5c2e4707a484706f7f37
Ben Elferink <ben@odigos.io>
2025-08-14 14:23:09 +0300

    feat: implement temporary product page redirection based on search parameters for improved navigation flow

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,8 +1,21 @@
 'use client';
 
+import { useEffect } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { Hero3, Hero4, InfoSections2, LearnMoreBlogs } from '@/containers';
 
 const Product = () => {
+  // TODO: remove this once we have a proper product page
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const shouldStay = searchParams.get('stay') != null;
+
+  useEffect(() => {
+    if (!shouldStay) router.push('/');
+  }, [router, shouldStay]);
+
+  if (!shouldStay) return null;
+
   return (
     <>
       <Hero4 />
