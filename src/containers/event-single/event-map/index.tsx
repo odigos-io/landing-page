c7217956f66e1e74fedbdf11d167da2bd6d1107c
Ben Elferink <ben@odigos.io>
2025-08-19 11:02:02 +0300

    feat: enhance EventMap component with advanced markers and dynamic map styles

--- src/containers/event-single/event-map/index.tsx
+++ src/containers/event-single/event-map/index.tsx
@@ -1,37 +1,102 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
+import { Text } from '@/components';
+import { useMobile } from '@/contexts';
 import { fetchCoordinates } from '@/functions';
 import { GOOGLE_MAPS_API_KEY } from '@/constants';
-import { APIProvider, Map } from '@vis.gl/react-google-maps';
+import styled, { DefaultTheme, useTheme } from 'styled-components';
+import { APIProvider, AdvancedMarker, InfoWindow, Map, Pin, useAdvancedMarkerRef } from '@vis.gl/react-google-maps';
 
 interface EventMapProps {
   location?: string;
 }
 
-export const EventMap = ({ location }: EventMapProps) => {
+enum MapTypeId {
+  HYBRID = 'hybrid',
+  ROADMAP = 'roadmap',
+  SATELLITE = 'satellite',
+  TERRAIN = 'terrain',
+}
+
+enum MapGestureHandling {
+  NONE = 'none',
+  AUTO = 'auto',
+  GREEDY = 'greedy',
+  COOPERATIVE = 'cooperative',
+}
+
+const IS_DARK = false;
+const MAP_CONFIG_INDEX = IS_DARK ? 1 : 0;
+
+const MAP_CONFIGS = [
+  {
+    label: 'Light',
+    mapId: '49ae42fed52588c3',
+    mapTypeId: MapTypeId.ROADMAP,
+    pin: () => <Pin background='#EA4335' borderColor='#B01D1D' glyphColor='#B01D1D' />,
+  },
+  {
+    label: 'Dark',
+    mapId: '739af084373f96fe',
+    mapTypeId: MapTypeId.ROADMAP,
+    pin: (theme: DefaultTheme) => <Pin background={theme.colors.purple} borderColor={theme.colors.purple_darker} glyphColor={theme.colors.cyan} />,
+  },
+];
+
+const MapWrapper = styled.div`
+  width: 100%;
+  height: 330px;
+  border-radius: 32px;
+  overflow: hidden;
+`;
+
+export const EventMap = ({ location: loc }: EventMapProps) => {
+  // trim the content in parentheses
+  const location = loc?.replace(/\s*\([^)]*\)/, '');
+
+  const theme = useTheme();
+  const { isMobile } = useMobile();
+
   const [coordinates, setCoordinates] = useState<{ lat: number; lng: number }>({ lat: 0, lng: 0 });
 
   useEffect(() => {
-    if (location) fetchCoordinates(location).then(setCoordinates).catch(console.error);
+    if (location)
+      fetchCoordinates(location)
+        .then((coordinates) => coordinates && setCoordinates(coordinates))
+        .catch(console.error);
   }, [location]);
+
+  const [markerRef, marker] = useAdvancedMarkerRef();
+  const [infoWindowOpen, setInfowindowOpen] = useState(true);
+  const toggleInfoWindow = () => setInfowindowOpen((prev) => !prev);
+
+  if (!coordinates.lat && !coordinates.lng) return null;
 
   return (
     <APIProvider apiKey={GOOGLE_MAPS_API_KEY}>
-      {/* TODO: configure this after obtaining an API KEY */}
-      <Map
-        defaultCenter={{
-          lat: coordinates.lat,
-          lng: coordinates.lng,
-        }}
-        defaultZoom={3}
-        gestureHandling='greedy'
-        disableDefaultUI={true}
-        style={{
-          width: '100%',
-          height: '100%',
-        }}
-      />
+      <MapWrapper>
+        <Map
+          // The map ID determines the map style.
+          // For more info, see: https://developers.google.com/maps/documentation/javascript/map-ids/mapid-over
+          // For examples, see: https://codesandbox.io/p/sandbox/github/visgl/react-google-maps/tree/main/examples/change-map-styles?file=%2Fsrc%2Fapp.tsx
+          mapId={MAP_CONFIGS[MAP_CONFIG_INDEX].mapId}
+          mapTypeId={MAP_CONFIGS[MAP_CONFIG_INDEX].mapTypeId}
+          defaultCenter={coordinates}
+          defaultZoom={4}
+          gestureHandling={isMobile ? MapGestureHandling.COOPERATIVE : MapGestureHandling.GREEDY}
+          disableDefaultUI
+          style={{
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          <AdvancedMarker ref={markerRef} position={coordinates} title={location} clickable onClick={toggleInfoWindow}>
+            {MAP_CONFIGS[MAP_CONFIG_INDEX].pin(theme)}
+            {infoWindowOpen && <InfoWindow anchor={marker} onCloseClick={toggleInfoWindow} headerContent={<Text color={theme.colors.black}>{location}</Text>} />}
+          </AdvancedMarker>
+        </Map>
+      </MapWrapper>
     </APIProvider>
   );
 };
--- src/functions/fetchCoordinates.ts
+++ src/functions/fetchCoordinates.ts
@@ -1,30 +1,54 @@
 import { GOOGLE_MAPS_API_KEY } from '@/constants';
 
-interface GoogleApiResponse {
-  status: string;
-  error_message: string;
-  results: {
-    geometry: {
-      location: {
-        lat: number;
-        lng: number;
-      };
-    };
-  }[];
+export interface LatLng {
+  lat: number;
+  lng: number;
 }
 
-export const fetchCoordinates = async (location: string) => {
-  let coordinates = { lat: 0, lng: 0 };
+interface LatitudeLongitude {
+  latitude: number;
+  longitude: number;
+}
 
+interface GoogleLocation {
+  place_id: string;
+  formatted_address: string;
+  address_components: {
+    long_name: string;
+    short_name: string;
+    types: string[];
+  }[];
+  geometry: {
+    location_type: string;
+    location: LatLng;
+    bounds: {
+      northeast: LatLng;
+      southwest: LatLng;
+    };
+    viewport: {
+      northeast: LatLng;
+      southwest: LatLng;
+    };
+  };
+  navigation_points: {
+    location: LatitudeLongitude;
+  }[];
+  types: string[];
+}
+
+interface GoogleApiResponse {
+  status: string;
+  error_message?: string;
+  results: GoogleLocation[];
+}
+
+export const fetchCoordinates = async (location: string): Promise<LatLng | null> => {
   try {
     const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(location)}&key=${GOOGLE_MAPS_API_KEY}`);
     const data = (await response.json()) as GoogleApiResponse;
 
-    // TODO: test this after obtaining an API KEY
-    console.log(data);
-
     if (data.results.length) {
-      coordinates = data.results[0].geometry.location;
+      return data.results[0].geometry.location;
     } else {
       console.warn(data.status, data.error_message);
     }
@@ -32,5 +56,5 @@ export const fetchCoordinates = async (location: string) => {
     console.error(error);
   }
 
-  return coordinates;
+  return null;
 };
