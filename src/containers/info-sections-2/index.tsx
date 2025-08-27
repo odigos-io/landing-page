a130910eec3dbd819d2ea4cc86a28a4e0c353ef9
Ben Elferink <ben@odigos.io>
2025-08-27 12:17:48 +0300

    refactor: clean up product page logic and improve header styling

--- src/containers/info-sections-2/index.tsx
+++ src/containers/info-sections-2/index.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import React from 'react';
+import Image from 'next/image';
 import styled from 'styled-components';
 import { useMobile } from '@/contexts';
+import { TextLayers } from '@/components';
 import { INFO_SECTIONS_2 } from '@/constants';
-import { Render3D, TextLayers } from '@/components';
 import { ConstrainedWrapper, FlexColumn } from '@/styles';
 
 const Section = styled.section<{ $isMobile: boolean; $inverted: boolean }>`
@@ -16,7 +17,7 @@ const Section = styled.section<{ $isMobile: boolean; $inverted: boolean }>`
   gap: 12px;
 `;
 
-const Wrap3D = styled.div<{ $isMobile: boolean }>`
+const WrapImage = styled.div<{ $isMobile: boolean }>`
   width: ${({ $isMobile }) => ($isMobile ? '100%' : 'unset')};
   display: ${({ $isMobile }) => ($isMobile ? 'flex' : 'unset')};
   justify-content: ${({ $isMobile }) => ($isMobile ? 'center' : 'unset')};
@@ -29,13 +30,13 @@ export const InfoSections2 = () => {
   return (
     <ConstrainedWrapper $isMobile={isMobile}>
       <FlexColumn $gap={isMobile ? 32 : 64}>
-        {INFO_SECTIONS_2.map(({ title, descriptions, bullets, threeDAsset }, i) => (
+        {INFO_SECTIONS_2.map(({ title, descriptions, bullets, imageSrc }, i) => (
           <Section key={`section-${title}`} $isMobile={isMobile} $inverted={i % 2 === 1}>
             <TextLayers title={title} descriptions={descriptions} bullets={bullets} />
 
-            <Wrap3D $isMobile={isMobile}>
-              <Render3D scene={threeDAsset} width={isMobile ? 300 : 580} height={isMobile ? 300 : 580} />
-            </Wrap3D>
+            <WrapImage $isMobile={isMobile}>
+              <Image src={imageSrc} alt={title} width={isMobile ? 300 : 580} height={isMobile ? 300 : 580} />
+            </WrapImage>
           </Section>
         ))}
       </FlexColumn>
