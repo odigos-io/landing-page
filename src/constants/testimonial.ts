5158c7b14df2f6440bc5dfa337aba9b2cab26b48
Ben Elferink <ben@odigos.io>
2025-10-19 10:30:11 +0300

    feat: update testimonial

--- src/constants/testimonial.ts
+++ src/constants/testimonial.ts
@@ -1,4 +1,4 @@
 export const TESTIMONIAL_QUOTE =
-  '“Odigos has proven to be a game-changer for us. With its ability to generate distributed traces instantly, Odigos has streamlined our process of identifying and resolving performance issues, pinpointing bottlenecks within microservices communication, and enhancing the overall reliability of our applications.”';
-export const TESTIMONIAL_AUTHOR_NAME = 'Tolu Aina';
-export const TESTIMONIAL_AUTHOR_POSITION = 'Access Bank';
+  '“eBPF-based instrumentation is clearly the future for observability. It brings composability to the observability stack and a is much faster way to get metrics, logs, and traces to different data platforms and gives us more control over our overall observability strategy. With Odigos we make the lives of our development teams dramatically/so much easier. Odigos integrates seamlessly with our existing stack, sending traces, metrics, and logs straight into Splunk. It’s been a great way to expand observability across our services with almost zero developer overhead.”';
+export const TESTIMONIAL_AUTHOR_NAME = 'Sebastian Bonk';
+export const TESTIMONIAL_AUTHOR_POSITION = 'Avodaq';
