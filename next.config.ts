8bd02822826ee0d2197981eea8e0ab99374dbf52
Ben Elferink <ben@odigos.io>
2025-10-21 16:18:28 +0300

    chore: add eslint directive for path alias in webpack configuration

--- next.config.ts
+++ next.config.ts
@@ -63,6 +63,7 @@ const nextConfig: NextConfig = {
     // Optimize imports for better tree shaking
     config.resolve.alias = {
       ...config.resolve.alias,
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
       '@': require('path').resolve(__dirname, 'src'),
     };
 
