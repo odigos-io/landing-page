d733f2ecdda9fb6fd04fa62d78a4e8c769542ae9
Ben Elferink <ben@odigos.io>
2025-12-10 10:20:30 +0200

    chore: update Next.js configuration and dependencies for improved performance and compatibility

--- next.config.ts
+++ next.config.ts
@@ -6,69 +6,23 @@ const nextConfig: NextConfig = {
     styledComponents: true,
   },
   experimental: {
-    optimizePackageImports: ['@splinetool/react-spline', 'styled-components', 'react', 'react-dom'],
+    optimizePackageImports: [
+      '@odigos/ui-kit',
+      '@splinetool/react-spline',
+      '@splinetool/runtime',
+      '@uiw/react-markdown-preview',
+      '@vis.gl/react-google-maps',
+      'gray-matter',
+      'next',
+      'next-plausible',
+      'react',
+      'react-dom',
+      'react-type-animation',
+      'styled-components',
+      'zustand',
+    ],
   },
-  webpack: (config, { dev, isServer }) => {
-    // Optimize chunk splitting and tree shaking
-    if (!dev && !isServer) {
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        minSize: 20000,
-        maxSize: 244000,
-        cacheGroups: {
-          default: {
-            minChunks: 2,
-            priority: -20,
-            reuseExistingChunk: true,
-          },
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name: 'vendors',
-            priority: -10,
-            chunks: 'all',
-          },
-          spline: {
-            test: /[\\/]node_modules[\\/]@splinetool[\\/]/,
-            name: 'spline',
-            priority: 20,
-            chunks: 'all',
-          },
-          styledComponents: {
-            test: /[\\/]node_modules[\\/]styled-components[\\/]/,
-            name: 'styled-components',
-            priority: 15,
-            chunks: 'all',
-          },
-          react: {
-            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
-            name: 'react',
-            priority: 10,
-            chunks: 'all',
-          },
-          common: {
-            name: 'common',
-            minChunks: 2,
-            priority: 5,
-            chunks: 'all',
-            reuseExistingChunk: true,
-          },
-        },
-      };
-
-      // Enable tree shaking
-      config.optimization.usedExports = true;
-      config.optimization.sideEffects = false;
-    }
-
-    // Optimize imports for better tree shaking
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      // eslint-disable-next-line @typescript-eslint/no-require-imports
-      '@': require('path').resolve(__dirname, 'src'),
-    };
-
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
